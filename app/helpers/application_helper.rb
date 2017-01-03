75227ce1fa1de013d7eaca08cbbf5d45560af272
Im Genius <zaeem.asif@gmail.com>
2017-01-03 20:35:15 +0500

    Added avatar_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def avatar_url(user, size = 500, tag_class = 'img-responsive', radius = '',
+    gravity = '')
+    image = retrieve_profile_image user, size
+    image_tag image, width: size, height: size, class: tag_class
+  end
+
+  private
+    def retrieve_profile_image(user, size)
+      if user.image_url.present?
+        user.image_url
+      else
+        gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+        "https://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=identicon"
+      end
+    end
 end
