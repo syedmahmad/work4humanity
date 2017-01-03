76024f84a1c666a9e83b109c4ff9e653fcd5027e
zaeem <devzaeem.asif@gmail.com>
2017-01-03 15:35:33 +0500

    working on authentication

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,6 +12,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
+
+  config.omniauth :facebook, "KEY", "SECRET"
+
+  
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
   # Configure the class responsible to send e-mails.
