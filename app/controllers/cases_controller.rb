4e62a6511682e93305b01a155ab2a4d765e5ddcb
Adil Mahmood <devadil.mahmood@gmail.com>
2017-01-03 14:21:37 +0500

    setting up basic structure for cases

---
+++ app/controllers/cases_controller.rb
@@ -0,0 +1,15 @@
+class CasesController < ApplicationController
+
+	def index
+
+	end
+
+	def create
+			
+	end
+
+	def edit
+			
+	end	
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103080827) do
+ActiveRecord::Schema.define(version: 20170103091420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cases", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "amount_required"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
