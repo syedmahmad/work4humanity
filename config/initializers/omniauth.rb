460aae20127b1a56f4419121bc6ba4f4b923cfef
Im Genius <zaeem.asif@gmail.com>
2017-01-03 17:39:07 +0500

    Fixed omniAuth ENV issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['368123226881871'], ENV['8e96d2e97691430047f96856adde34b5']
+  provider :facebook, '368123226881871', '8e96d2e97691430047f96856adde34b5'
 end
