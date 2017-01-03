ef21de40c1310119c4d9d3b62907e57c2ef5a91e
Adil Mahmood <devadil.mahmood@gmail.com>
2017-01-03 16:51:08 +0500

    path for public file uploads updated

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,6 @@
 class Attachment < ActiveRecord::Base
 	belongs_to :case
-	has_attached_file :attached_file
+	has_attached_file :attached_file, :path => "uploaded_attachments/:class/:attachment/:id_partition/:style/:filename"
 	validates :attached_file, :attachment_presence => true
 	validates_attachment :attached_file, content_type: { content_type: /\Aimage\/.*\Z/ }
 end
