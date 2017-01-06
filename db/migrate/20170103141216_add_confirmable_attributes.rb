737274289772049a3632507a12af0526f24893c2
Adil Mahmood <devadil.mahmood@gmail.com>
2017-01-06 18:32:17 +0500

    migration updated for production

--- db/migrate/20170103141216_add_confirmable_attributes.rb
+++ db/migrate/20170103141216_add_confirmable_attributes.rb
@@ -1,8 +1,8 @@
 class AddConfirmableAttributes < ActiveRecord::Migration
   def change
-  	add_column :users, :confirmation_token, :string
-  	add_column :users, :confirmed_at, :datetime
-  	add_column :users, :confirmation_sent_at, :datetime
-  	add_column :users, :unconfirmed_email, :string
+  	add_column :users, :confirmation_token, :string unless User.column_names.include? 'confirmation_token'
+  	add_column :users, :confirmed_at, :datetime unless User.column_names.include? 'confirmed_at'
+  	add_column :users, :confirmation_sent_at, :datetime unless User.column_names.include? 'confirmation_sent_at'
+  	add_column :users, :unconfirmed_email, :string unless User.column_names.include? 'unconfirmed_email'
   end
 end
