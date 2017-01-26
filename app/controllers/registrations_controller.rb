ae02bf51faf24a019459821b5f181bb69ec652c5
zaeem <devzaeem.asif@gmail.com>
2017-01-26 20:05:18 +0500

    changes applies

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     phone = params[:user][:mobile_number].gsub(/[^0-9a-z ]/i, '')
-    if phone[0 .. 1] == "92"
-      params[:user][:mobile_number] = "0"+phone[2 .. -1]
-    else
-      params[:user][:mobile_number] = phone
-    end
+    params[:user][:mobile_number] = phone
     path = :back
     user = User.find_by_mobile_number(params[:user][:mobile_number])
     if user
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += %w()
+page_specific_js_files = %w( phone_validation.js )
+Rails.application.config.assets.precompile += page_specific_js_files.map! {|path| "page_specific/" + path }
