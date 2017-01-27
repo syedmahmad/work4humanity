c4fa6f8ffee8bc1eca9e4a101e71d6de9dc8e14f
zaeem <devzaeem.asif@gmail.com>
2017-01-27 16:49:20 +0500

    changing content

--- app/helpers/cases_helper.rb
+++ app/helpers/cases_helper.rb
@@ -2,6 +2,8 @@ module CasesHelper
   def redirect_user_path
     if Rails.application.routes.recognize_path(request.referer)[:controller].downcase == "users"
       cases_user_path(current_user)
+    elsif Rails.application.routes.recognize_path(request.referer)[:controller].downcase == "logs"
+      logs_path
     else
       cases_path
     end
